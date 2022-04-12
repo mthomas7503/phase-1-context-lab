fb733f7449ec429966ef795d68e08674e548acfd
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2022-04-12 14:47:38 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
-/* Your Code Here */
+function createEmployeeRecord(array) {
+    function recordCreate(array) {
+    this.firstName = array[0],
+    this.familyName = array[1],
+    this.title = array[2],
+    this.payPerHour = array[3]
+    this.timeInEvents = [],
+    this.timeOutEvents = []}
+    let employeeRecord = new recordCreate(array);
+    return employeeRecord
+};
 
+function createEmployeeRecords(arrOfarr) {
+   return arrOfarr.map(x => createEmployeeRecord(x))
+}
+
+
+
+function createTimeInEvent(timeIn) {
+this.timeInEvents.push({type: "TimeIn",
+hour: parseInt(timeIn.substring(10), 10),
+date: timeIn.substring(0, 10)})
+return this}
+
+function createTimeOutEvent(timeOut) {
+this.timeOutEvents.push({type: "TimeOut",
+hour: parseInt(timeOut.substring(10), 10),
+date: timeOut.substring(0, 10)})
+return this
+}
+
+function hoursWorkedOnDate(ymd){
+    let timeIn = this.timeInEvents[0].hour
+    let timeOut = this.timeOutEvents[0].hour
+    if (ymd === this.timeInEvents[0].date){
+        if (timeIn > timeOut) {return (timeIn - timeOut)/100}
+        else if (timeOut > timeIn) {return(timeOut - timeIn)/100}
+    }
+}
+
+function wagesEarnedOnDate(ymd) {
+    let hoursWorked = hoursWorkedOnDate.bind(this)
+  return  hoursWorked(ymd) * parseInt(this.payPerHour, 10)
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +63,13 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName (srcArray, fn) {
+    let src = srcArray
+  let emp = createEmployeeRecords(src);
+  let foundEmployee = emp.find(x => x.firstName === fn);
+  return foundEmployee
+}
+
+function calculatePayroll(arr) {
+    arr.reduce(() => {allWagesFor})
+}
